19e0831bb839db9d952a82e4a5d40c41725adebf
akowalew <ram.techen@gmail.com>
2019-05-25 13:10:51 +0200

    Added logging into Stemmer

--- Analyzer/src/main/scala/review/sentiment/analysis/preprocessing/Stemmer.scala
+++ Analyzer/src/main/scala/review/sentiment/analysis/preprocessing/Stemmer.scala
@@ -12,9 +12,7 @@ object Stemmer {
   def props: Props = Props[Stemmer]
 
   final case class StemmingRequest(rawText: String)
-
   final case class StemmingResponse(processedText: Array[String])
-
 }
 
 class Stemmer extends Actor with ActorLogging {
@@ -23,19 +21,18 @@ class Stemmer extends Actor with ActorLogging {
 
   private val tokenizer = """(?u)\b\p{L}+\b""".r
   private val stemmer = new PolishStemmer()
-  private val stopWords = Source.fromResource("polish-stopwords.txt")
-    .getLines
-    .toSet
+  private val stopWords = loadStopWords()
 
   override def receive: Receive = {
     case StemmingRequest(rawText: String) =>
-
+      log.info(s"Stemming raw text of size: ${rawText.size}...")
       val tokens = tokenizer.findAllIn(rawText).toArray
       val processedText = tokens
         .map(token => token.toLowerCase())
         .map(token => stem(token))
         .filter(token => !isStopWord(token))
 
+      log.info(s"Processed text: ${processedText.mkString(" ")}")
       sender() ! StemmingResponse(processedText)
   }
 
@@ -50,5 +47,11 @@ class Stemmer extends Actor with ActorLogging {
 
   private def isStopWord(token: String): Boolean = {
     stopWords.contains(token)
+  }
+
+  private def loadStopWords(): Set[String] = {
+    Source.fromResource("polish-stopwords.txt")
+      .getLines
+      .toSet
   }
 }
