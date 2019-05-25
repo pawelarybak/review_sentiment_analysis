e95e94f09ab0ec01f83390946aefa1a11ea76766
akowalew <ram.techen@gmail.com>
2019-05-25 19:48:16 +0200

    Main: Added training of AnalysisManager with fetched dummy reviews data

--- Analyzer/src/main/scala/review/sentiment/analysis/Main.scala
+++ Analyzer/src/main/scala/review/sentiment/analysis/Main.scala
@@ -1,20 +1,51 @@
 package review.sentiment.analysis
 
-import akka.actor.{ActorRef, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Success, Failure}
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.pattern.{ask, pipe}
+import akka.util.Timeout
+
 import review.sentiment.analysis.api.HttpServerActor
 import review.sentiment.analysis.api.HttpServerActor.StartServer
 import review.sentiment.analysis.manager.AnalysisManager
+import review.sentiment.analysis.manager.AnalysisManager.{AddReviewsRequest, AddReviewsResponse}
 
 object Main extends App {
-
+    println("Initializing system...")
     val config = ConfigFactory.load()
     val system = ActorSystem("rsa-system", config)
-
-    println("Hello")
-
-    val analysisManager : ActorRef = system.actorOf(Props[AnalysisManager], "analysis_manager")
+    val analysisManager = system.actorOf(Props(new AnalysisManager()), "analysis_manager")
     val httpServer = system.actorOf(Props(new HttpServerActor(analysisManager)), "http_server")
 
-    httpServer ! StartServer
+    private implicit val timeout = Timeout(5 seconds)
+    private implicit val ec = ExecutionContext.global
+
+    println("Training AnalysisManager...")
+    val reviews = fetchReviews()
+    val training = analysisManager.ask(AddReviewsRequest(reviews))
+                                  .mapTo[AddReviewsResponse]
+    training onComplete {
+        case Success(response) =>
+            println(s"Train complete. New words count: ${response.newWordsCount}, accuracy: ${response.accuracy}")
+
+            println("Starting HttpServer...")
+            httpServer ! StartServer
+
+        case Failure(t) =>
+            println(s"Train error: ${t.getMessage()}")
+    }
+
+    def fetchReviews(): Array[(String, Int)] = {
+        Array(
+            ("Spoko spoko", 10),
+            ("Fajne to", 8),
+            ("Eeee kiepskie", 2),
+            ("Nuda", 1)
+        )
+    }
 }
