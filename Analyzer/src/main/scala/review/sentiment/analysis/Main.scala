51ba95379071cb012d54098443c46bcbad4a2891
akowalew <ram.techen@gmail.com>
2019-05-31 10:06:59 +0200

    Main: Fixed startup procedure of the program

--- Analyzer/src/main/scala/review/sentiment/analysis/Main.scala
+++ Analyzer/src/main/scala/review/sentiment/analysis/Main.scala
@@ -1,22 +1,12 @@
 package review.sentiment.analysis
 
 import akka.actor.{ActorSystem, Props}
-import akka.pattern.ask
-import akka.util.Timeout
 
 import com.typesafe.config.ConfigFactory
-import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.duration._
-import scala.language.postfixOps
+import review.sentiment.analysis.manager.AnalysisManager.InitializeRequest
 
 import org.apache.spark.sql.SparkSession
-
-import review.sentiment.analysis.api.HttpServerActor
-import review.sentiment.analysis.api.HttpServerActor.StartServer
-import review.sentiment.analysis.manager.AnalysisManager
-import review.sentiment.analysis.manager.AnalysisManager.{InitializeRequest, InitializeResponse}
 
 object Spark {
     val session = SparkSession.builder()
@@ -31,23 +21,13 @@ object Spark {
 }
 
 object Main extends App {
-    println("Initializing system...")
+
     val config = ConfigFactory.load()
     val system = ActorSystem("rsa-system", config)
+
     Spark.ctx.setLogLevel("WARN")
 
     val supervisor = system.actorOf(Props[MainSupervisor], "supervisor")
-    val analysisManager = system.actorOf(AnalysisManager.props, "analysis_manager")
-    val httpServer = system.actorOf(Props(new HttpServerActor(analysisManager)), "http_server")
-
-    private implicit val timeout = Timeout(99999 seconds)
-    private implicit val ec = ExecutionContext.global
 
     supervisor ! InitializeRequest
-
-    analysisManager
-        .ask(InitializeRequest())
-        .mapTo[InitializeResponse]
-        .map(_ => httpServer ! StartServer)
-        .map(_ => println("Initialization finished!"))
 }
